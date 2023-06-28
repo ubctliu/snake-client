13d861184bd0fd9f82ed298ea31626ce3c95817d
Terrence <terrence.liu@alumni.ubc.ca>
2023-06-27 17:04:22 -0700

    added setupInput + handleUserInput functions to support user inputevent handlers from keyboard

--- play.js
+++ play.js
@@ -1,4 +1,31 @@
+const { stdin } = require("process");
 const {connect} = require("./client");
 
 console.log("Connecting...");
 connect();
+
+// set up interface to handle user input from stdin
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume;
+  return stdin;
+};
+
+stdin.on("data", (data) => {
+  handleUserInput(data);
+});
+
+const handleUserInput = (key) => {
+
+  switch (key) {
+  // if ctrl + c, terminate
+  case "\u0003": process.exit();
+    break;
+  }
+  
+};
+
+setupInput();
