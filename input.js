f74c88aa0a1da70680204d14dfdb19e1abe9aae3
Terrence <terrence.liu@alumni.ubc.ca>
2023-06-27 17:48:28 -0700

    connected the game and the input event handlers, making the game playable & added movement for wasd

--- input.js
+++ input.js
@@ -1,6 +1,10 @@
+// stores the active TCP connection object
+let connection;
+
 // set up interface to handle user input from stdin
-const setupInput = () => {
+const setupInput = (conn) => {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
@@ -8,15 +12,34 @@ const setupInput = () => {
   return stdin;
 };
 
+// handles movement inputs + exit inputs
 const handleUserInput = (key) => {
 
   switch (key) {
+
   // if ctrl + c, terminate
   case "\u0003":
     process.exit();
     break;
+    
+  case "w":
+    connection.write("Move: up");
+    break;
+
+  case "a":
+    connection.write("Move: left");
+    break;
+
+  case "s":
+    connection.write("Move: down");
+    break;
+
+  case "d":
+    connection.write("Move: right");
+    break;
+
   default:
-    console.log(`Pressed ${key}`);
+    break;
   }
   
 };
